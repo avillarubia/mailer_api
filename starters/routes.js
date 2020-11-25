0a89cd1c6bd914168e108a75d97c8de5a2fdd3bf
aljohn <johnnjill3@gmail.com>
2020-11-25 22:21:38 +0800

    removing authentications and mongodb connections

--- starters/routes.js
+++ starters/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const config = require('config')
-const auth = require('../routes/auth')
-const users = require('../routes/users')
+// const auth = require('../routes/auth')
+// const users = require('../routes/users')
 const mailer = require('../routes/mailer')
 const error = require('../middlewares/error')
 
@@ -11,8 +11,8 @@ module.exports = function (app) {
     const path = `/api/${version}`
 
     app.use(express.json())
-    app.use(`${path}/auth`, auth)
-    app.use(`${path}/users`, users)
+    // app.use(`${path}/auth`, auth)
+    // app.use(`${path}/users`, users)
     app.use(`${path}/mailer`, mailer)
     app.use(error)
 }
